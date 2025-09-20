0eed7f0109a7e0af2e016b0e112de851c62a8c9d
Tangjihang <tjh20061117@qq.com>
2025-09-20 18:11:11 +0800

    seventh

--- sum02.c
+++ sum02.c
@@ -14,6 +14,32 @@ int exist(int arr[], int count, int num)
     return 0;
 }
 
+// get the average number
+int average(int arr[])
+{
+    double sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += arr[i];
+    }
+    double aver = sum / 10;
+    printf("the average of this array is: %d\n", aver);
+    return aver;
+}
+
+//count how many nums are lower than the average?
+int count(int arr[],int average)
+{
+    int countNum = 0;
+    for(int i = 0;i < 10;i ++)
+    {
+        if(arr[i] < average)
+        {
+            countNum ++;
+        }
+    }
+}
+
 int main()
 {
     srand((unsigned int)time(NULL));
@@ -24,14 +50,19 @@ int main()
         do
         {
             num = rand() % 100 + 1;
-            printf("wowowowo%d\n",num);
+            printf("wowowowo%d\n", num);
         } while (exist(arr, i, num));
         arr[i] = num;
     }
+
     for (int i = 0; i < 10; i++)
     {
         printf("%d\n", arr[i]);
     }
 
+    average(arr);
+
+    count(arr,average(arr));
+    
     return 0;
 }
