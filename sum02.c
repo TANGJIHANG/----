cae7ded6be0f020e13834e0d2a4b733ae4ce281f
Tangjihang <tjh20061117@qq.com>
2025-09-20 14:17:43 +0800

    forth

--- sum02.c
+++ sum02.c
@@ -2,44 +2,33 @@
 #include <time.h>
 
 // check if any num is the same:
-int check(int arr[], int count, int num)
+int exist(int arr[], int count, int num)
 {
-    if (arr[count] == num)
+    for (int i = 0; i < count; i++)
     {
-        return 1;
+        if (num == arr[i])
+        {
+            return 1;
+        }
     }
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
 
 int main()
 {
     srand((unsigned int)time(NULL));
-    // produce 10 different nums
-
     int arr[10];
-    int count = 0;
-    for (int i = 0; i < 10; i++)
+    for(int i = 0;i < 10;i++)
     {
-        //生成一个随机数，假设此时为i=0
         int num = rand()%100 + 1;
-        
-        int a = 0;
-        //检查与前面的有没有重复
-        for(int i = 0;i < count + 1;i++)
+        while(!exist(arr,i,num))
         {
-            if(!check(arr,count,num))
-            {
-                a++;
-            }
+            arr[i] = num;
         }
-        if(a == count)
-        {
-            arr[count] == num;
-        }
-        count ++;
+    }
+    for(int i = 0;i < 10;i++)
+    {
+        printf("%d\n",arr[i]);
     }
 
     return 0;
