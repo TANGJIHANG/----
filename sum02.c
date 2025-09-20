f69a3dba5e068bf6dbd85040b4a8a08dea143789
Tangjihang <tjh20061117@qq.com>
2025-09-20 10:39:53 +0800

    second

--- sum02.c
+++ sum02.c
@@ -16,25 +16,34 @@ int check(int num1,int num2)
 
 int main()
 {
+    srand((unsigned int)time(NULL));
     //produce 10 different nums
 
-    srand((unsigned int)time(NULL));
+
+    //先生成
+    //判断成功后，把它存入数组
     int arr[10];
-    for(int i = 0;i < 10;i++)
+    arr[0] = rand();
+    //第一个数不可能重复
+    //第二个数需要判断
+    
+    do
     {
-        arr[i] = rand()%100 + 1;
-    }
-    for(int i = 0;i < 10;i++)
+        int num1 = rand();
+        arr[1] = num1;
+    }while(check(arr[1],arr[0]));
+    //当while括号中为0是可以停止，
+
+    //第三个数
+    do
     {
-        for(int j = i + 1;j < 10;j++)
-        {
-            if(check(arr[i],arr[j]) == 1)
-            {
-                arr[j] = rand()%100 + 1;
-                printf("there are same nums");
-            }
-        }
-    }
+        int num2 = rand();
+        arr[2] = num2;
+    }while(check(arr[2],arr[1]))
+    //问题check函数要滚动循环
+
+
+
 
     return 0;
 }
