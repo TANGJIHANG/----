78531ae3d462b719ab17f9520aaaa56cf886fbcb
Tangjihang <tjh20061117@qq.com>
2025-09-20 14:10:47 +0800

    third

--- sum02.c
+++ sum02.c
@@ -2,18 +2,16 @@
 #include <time.h>
 
 // check if any num is the same:
-int check(int num[], int arr[], int count)
-// count代表了num口号中的编号
+int check(int arr[], int count, int num)
 {
-    int a = 0;
-    for (int i = 0; i < count; i++)
+    if (arr[count] == num)
     {
-        if (num[count] == arr[i])
-        {
-            a++;
-        }
+        return 1;
     }
-    return a;
+    else
+    {
+        return 0;
+    }
 }
 
 int main()
@@ -21,27 +19,27 @@ int main()
     srand((unsigned int)time(NULL));
     // produce 10 different nums
 
-    // 先生成
-    // 判断成功后，把它存入数组
-    int num[10];
     int arr[10];
-    arr[0] = rand();
-    // 第一个数不可能重复
-    // 第二个数需要判断
-
-
-    for (int i = 1; i < 10; i++)
+    int count = 0;
+    for (int i = 0; i < 10; i++)
     {
-        do
+        //生成一个随机数，假设此时为i=0
+        int num = rand()%100 + 1;
+        
+        int a = 0;
+        //检查与前面的有没有重复
+        for(int i = 0;i < count + 1;i++)
         {
-            num[i] = rand();
-            arr[i] = num[i];
-        } while (num, arr, i);
-    }
-
-    for(int i = 0;i < 10; i++)
-    {
-        printf("%d\n",arr[i]);
+            if(!check(arr,count,num))
+            {
+                a++;
+            }
+        }
+        if(a == count)
+        {
+            arr[count] == num;
+        }
+        count ++;
     }
 
     return 0;
